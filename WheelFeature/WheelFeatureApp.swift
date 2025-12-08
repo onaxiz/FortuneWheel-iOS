f3e20931cb6c17c7290d600209502683c4aeb169
emaksimova <maksimovaswift@gmail.com>
2025-12-08 22:20:24 +0300

    Initial Commit

---
+++ WheelFeature/WheelFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelFeatureApp.swift
+//  WheelFeature
+//
+//  Created by Евгения Максимова on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct WheelFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
